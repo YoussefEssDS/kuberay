1c6d8664e525335cf16e4a341878789766a46295
Youssef Esseddiq <youssef.esseddiq.ouatiti@huawei.com>
2024-10-29 12:22:27 -0400

    Migrate testing of TSP from GHA to buildkite

--- ray-operator/test/sampleyaml/raycluster_test.go
+++ ray-operator/test/sampleyaml/raycluster_test.go
@@ -2,9 +2,11 @@ package sampleyaml
 
 import (
 	"testing"
+	"time"
 
 	"github.com/onsi/gomega"
 
+	v1 "k8s.io/api/core/v1"
 	rayv1 "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
 	. "github.com/ray-project/kuberay/ray-operator/test/support"
 )
@@ -102,37 +104,38 @@ func TestRayClusterTopologySC(t *testing.T) {
 		},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			test := With(t)
-			namespace := test.NewTestNamespace()
-			test.StreamKubeRayOperatorLogs()
+	t.Run(tt.name, func(t *testing.T) {
+		test := With(t)
+		namespace := test.NewTestNamespace()
+		test.StreamKubeRayOperatorLogs()
+		rayClusterFromYaml := DeserializeRayClusterSampleYAML(test, tt.name)
+		KubectlApplyYAML(test, tt.name, namespace.Name)
 
-			rayClusterFromYaml := DeserializeRayClusterSampleYAML(test, tt.name)
-			KubectlApplyYAML(test, tt.name, namespace.Name)
+		rayCluster := GetRayCluster(test, namespace.Name, rayClusterFromYaml.Name)
+		test.Expect(rayCluster).NotTo(gomega.BeNil())
 
-			rayCluster := GetRayCluster(test, namespace.Name, rayClusterFromYaml.Name)
-			test.Expect(rayCluster).NotTo(gomega.BeNil())
+		test.T().Logf("Waiting for RayCluster %s/%s to be ready", namespace.Name, rayCluster.Name)
+		test.Eventually(RayCluster(test, namespace.Name, rayCluster.Name), TestTimeoutMedium).
+			Should(gomega.WithTransform(RayClusterState, gomega.Equal(rayv1.Ready)))
 
-			test.T().Logf("Waiting for RayCluster %s/%s to be ready with topology constraints", namespace.Name, rayCluster.Name)
-			test.Eventually(RayCluster(test, namespace.Name, rayCluster.Name), TestTimeoutMedium).
-				Should(gomega.WithTransform(RayClusterState, gomega.Equal(rayv1.Ready)))
+		// Check for 3 running pods as per the topology constraints
+		test.Eventually(GetWorkerPods(test, rayCluster), TestTimeoutShort).Should(gomega.WithTransform(AllPodsRunningAndReady, gomega.BeTrue()))
+		runningPods := GetWorkerPods(test, rayCluster)
+		test.Expect(len(runningPods)).To(gomega.Equal(3))
 
-			rayCluster = GetRayCluster(test, namespace.Name, rayCluster.Name)
+		// Consistently check that there are 6 pending pods due to topology constraints
+		test.Consistently(GetWorkerPods(test, rayCluster), TestTimeoutShort, time.Second).
+		Should(gomega.WithTransform(AllPodsPending, gomega.Equal(6)))
+	})
+}
 
-			// Verify the expected topology constraints results
-			expectedRunning := 3
-			expectedPending := 6
-
-			// Check if exactly 3 pods are in Running state eventually
-			test.Eventually(func() int {
-				return CountPodsInPhase(test, namespace.Name, rayCluster.Name, "Running")
-			}, TestTimeoutShort).Should(gomega.Equal(expectedRunning))
-
-			// Check if exactly 6 pods are consistently in Pending state
-			test.Consistently(func() int {
-				return CountPodsInPhase(test, namespace.Name, rayCluster.Name, "Pending")
-			}, TestTimeoutShort).Should(gomega.Equal(expectedPending))
-		})
+// Helper func to count pending pods
+func AllPodsPending(pods []*v1.Pod) int {
+	count := 0
+	for _, pod := range pods {
+		if pod.Status.Phase == v1.PodPending {
+			count++
+		}
 	}
+	return count
 }
